b6b0aa4358896bedbcff1ac6a7c3dd783bea3225
Yannis <yannis.samba@gmail.com>
2019-06-04 17:19:42 +0200

    fix property

--- JS/script.js
+++ JS/script.js
@@ -7,7 +7,9 @@ const information = () =>{
     events[i] = document.getElementsByClassName(body[i])
     
     events[i][0].addEventListener("mouseover",()=>{
-      events[i][1].removeAttribute("style")
+      // events[i][1].removeAttribute("style")
+      events[i][1].style="display:block"
+
     })
     events[i][0].addEventListener("mouseleave",()=>{
       events[i][1].style="display:none"
