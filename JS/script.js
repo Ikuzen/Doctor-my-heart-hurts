c173797f5f5e59209d17530e6831212a1bc6cca5
Yannis <yannis.samba@gmail.com>
2019-06-05 19:30:54 +0200

    format document, add some comments

--- JS/script.js
+++ JS/script.js
@@ -2,18 +2,15 @@ const information = () =>{
   const body = ["shoulder","hand","knee","arm","leg","foot"]
   const events =[]
 
-
+  // loop that gets the elements of said classes, and also add events on them
   for (let i=0; i<body.length;i++){
     events[i] = document.getElementsByClassName(body[i])
     
     events[i][0].addEventListener("mouseover",()=>{
-      // events[i][1].removeAttribute("style")
-      events[i][1].style="display:block"
-
+      events[i][1].removeAttribute("style")
     })
     events[i][0].addEventListener("mouseleave",()=>{
       events[i][1].style="display:none"
-      
     })
   }
 }
